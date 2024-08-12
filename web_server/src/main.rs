bd834b3304d1fb3359a9859cbeb19bf434f213f6
YXHXianYu <2943003@qq.com>
2024-08-12 19:48:24 +0800

    feat(web server): improve file input

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -4,16 +4,16 @@ use std::{
     net::{TcpListener, TcpStream},
     thread,
     time::Duration,
+    path::PathBuf,
 };
 
 use web_server::ThreadPool;
-
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let thread_pool = ThreadPool::new(4);
 
-    for stream in listener.incoming().take(2) {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         thread_pool.execute(|| {
@@ -35,7 +35,9 @@ fn handle_connection(mut stream: TcpStream) {
         _ => ("HTTP/1.1 400 NOT FOUND", "404.html"),
     };
 
-    let contents = fs::read_to_string(file_name).unwrap();
+    let file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("resources").join(file_name);
+
+    let contents =  fs::read_to_string(file_path).unwrap();
     let length = contents.len();
     let response =
         format!("{status_line}\nContent-Length: {length}\n\n{contents}");
